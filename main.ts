aeadad06c1b7669d2174e996390d294c9a5ec86e
fuvesi magor <47007141+kalandor122@users.noreply.github.com>
2023-01-25 12:13:26 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    radio.setGroup(90)
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(90)
+    radio.sendNumber(1)
+})
